c96d494ef0b6bb32aec014e686905424f056cfaf
Tiri <shaymusts@gmail.com>
2022-01-16 23:04:06 +0200

    initial Place Details set-up

---
+++ src/components/PlaceDetails/PlaceDetails.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+  
+}
+
+const PlaceDetails = (props: Props) => {
+  return (
+    <div>
+      <h1>Place Details</h1>
+    </div>
+  )
+}
+
+export default PlaceDetails
